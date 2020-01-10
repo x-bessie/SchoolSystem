765f85fc456efc46bbc43686400273da734cace3
x-bessie <qwas002@126.com>
2020-01-10 14:16:09 +0800

    公共包

---
+++ eladmin/eladmin-common/src/main/java/me/zhengjie/utils/SecurityUtils.java
@@ -0,0 +1,34 @@
+package me.zhengjie.utils;
+
+import cn.hutool.json.JSONObject;
+import me.zhengjie.exception.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * 获取当前登录的用户
+ * @author Zheng Jie
+ * @date 2019-01-17
+ */
+public class SecurityUtils {
+
+    public static UserDetails getUserDetails() {
+        UserDetails userDetails;
+        try {
+            userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        } catch (Exception e) {
+            throw new BadRequestException(HttpStatus.UNAUTHORIZED, "登录状态过期");
+        }
+        return userDetails;
+    }
+
+    /**
+     * 获取系统用户名称
+     * @return 系统用户名称
+     */
+    public static String getUsername(){
+        Object obj = getUserDetails();
+        return new JSONObject(obj).get("username", String.class);
+    }
+}
