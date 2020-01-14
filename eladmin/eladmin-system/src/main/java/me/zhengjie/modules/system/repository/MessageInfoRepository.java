5d6794d537e71b597f7875dc0f5ad17e70a98b1e
x-bessie <qwas002@126.com>
2020-01-14 14:49:44 +0800

    公告栏

--- eladmin/eladmin-system/src/main/java/me/zhengjie/modules/system/repository/MessageInfoRepository.java
+++ eladmin/eladmin-system/src/main/java/me/zhengjie/modules/system/repository/MessageInfoRepository.java
@@ -10,12 +10,12 @@ import java.util.List;
 import java.util.Map;
 
 /**
-* @author HUO
-* @date 2020-01-13
-*/
+ * @author HUO
+ * @date 2020-01-14
+ */
 @Repository
 public interface MessageInfoRepository extends JpaRepository<MessageInfo, Integer>, JpaSpecificationExecutor<MessageInfo> {
 
-   @Query(value="select title,content,status,create_time from message_info where status=1  ORDER BY  create_time  DESC limit 5",nativeQuery = true)
+    @Query(value = "select title,content,status,create_time from message_info where status=1  ORDER BY  create_time  DESC limit 5", nativeQuery = true)
     List<Map<String, Object>> queryMessageInfo();
 }
