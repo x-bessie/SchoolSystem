a99efea8dfc46bc02ad89d67ab894043e7190030
x-bessie <qwas002@126.com>
2020-02-01 17:50:06 +0800

    添加 查询授课教师接口

--- eladmin/eladmin-system/src/main/java/me/zhengjie/modules/course/repository/CourseInfoRepository.java
+++ eladmin/eladmin-system/src/main/java/me/zhengjie/modules/course/repository/CourseInfoRepository.java
@@ -3,10 +3,19 @@ package me.zhengjie.modules.course.repository;
 import me.zhengjie.modules.course.domain.CourseInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
 
 /**
 * @author HUO
 * @date 2020-01-16
 */
+@Repository
 public interface CourseInfoRepository extends JpaRepository<CourseInfo, Integer>, JpaSpecificationExecutor<CourseInfo> {
+
+    @Query(value="select * from  course_info  where   name=?1 and class_teacher=?2 ",nativeQuery = true)
+    List<Map<String, Object>> getCoruseTeacher(String name, String class_teacher);
 }
