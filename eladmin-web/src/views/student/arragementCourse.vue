d3b708d33df54bb153b6a3472e77f0136e1fa059
x-bessie <qwas002@126.com>
2020-02-02 22:53:02 +0800

    学生课程安排，对教师的评价

--- eladmin-web/src/views/student/arragementCourse.vue
+++ eladmin-web/src/views/student/arragementCourse.vue
@@ -1,6 +1,51 @@
 <template>
   <div class="app-container">
     <!--工具栏-->
+    <!--对话框表单-->
+    <el-dialog title="对教师评价" :visible.sync="dialogVisible" width="50%" :before-close="handleClose">
+      <span slot="footer" class="dialog-footer">
+        <!-- <el-table ref="table" :data="teacherData" stripe size="small" style="width: 100%;">
+          <el-table-column prop="name" label="课程名称" />
+          <el-table-column prop="class_teacher" label="授课教师" />
+          <el-table-column prop="class_teacher" label="评价">
+            <el-input v-model="teacherData.class_teacher" placeholder="请输入内容" @change="handleEdit(scope.$index, scope.row)" />
+          </el-table-column>
+        </el-table>-->
+        <el-form
+          ref="form"
+          :model="teacherData"
+          :inline="true"
+          stripe
+          size="small"
+          style="width: 70%;"
+        >
+          <el-form-item label="课程名称">
+            <el-input
+              v-model="teacherData.course_name"
+              :disabled="true"
+              placeholder="课程名称"
+              style="width: 300px;"
+            />
+          </el-form-item>
+          <el-form-item label="授课教师">
+            <el-input
+              v-model="teacherData.class_teacher"
+              :disabled="true"
+              placeholder="授课教师"
+              style="width: 300px;"
+            />
+          </el-form-item>
+          <el-form-item label="评价">
+            <el-input v-model="teacherData.memo" style="width: 300px;" />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit(teacherData)">提交</el-button>
+            <el-button @click="dialogVisible = false">关闭</el-button>
+          </el-form-item>
+        </el-form>
+        <!-- <el-button type="primary" @click="summitUp(scope.row)">提交</el-button> -->
+      </span>
+    </el-dialog>
     <div class="head-container">
       <!--学生课程表表格渲染-->
       <el-table ref="table" :data="tableData" size="small" style="width: 100%;">
@@ -11,30 +56,58 @@
         <el-table-column prop="class_code" label="课程代码" width="180" />
         <el-table-column prop="school_year" label="学年" width="180" />
         <el-table-column prop="tearm" label="学期" width="180" />
+        <el-table-column label="操作" width="150">
+          <template scope="scope">
+            <el-button size="small" @click="judgeTeachers(scope.$index, scope.row)">对教师评价</el-button>
+          </template>
+        </el-table-column>
       </el-table>
-      <!--分页组件-->
-      <pagination />
     </div>
   </div>
 </template>
 
 <script>
 import { getStudentCourseMessage } from '@/api/studentCourseInfo'
+import { InsertCommentByStudent } from '@/api/commentInfo'
 import moment from 'moment'
 
 export default {
   data() {
     return {
+      dialogVisible: false,
+      // 课程数据
       tableData: [{
+        username: '',
         course_name: '',
+        course_id: '',
         classtime: '',
         class_num: '',
         class_teacher: '',
+        teacher_id: '',
         class_code: '',
         school_year: '',
         tearm: '',
         create_time: ''
-      }]
+      }],
+      // 弹窗数据
+      teacherData: [{
+        username: '',
+        id: '',
+        class_id: '',
+        course_name: '',
+        // name: '',
+        // teacher_name: '',
+        class_teacher: '',
+        memo: ''
+      }],
+      // 评价数据
+      commentData: {
+        username: '',
+        class_id: '',
+        teacher_id: '',
+        teacher_name: '',
+        memo: ''
+      }
     }
   },
   mounted() {
@@ -46,6 +119,38 @@ export default {
         this.tableData = res
       })
     },
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => { })
+    },
+    judgeTeachers: function(index, row) {
+      var _this = this
+      _this.teacherData = row
+      _this.dialogVisible = true
+    },
+    onSubmit(data) {
+      console.log(data)
+      if (this.teacherData.memo) {
+        InsertCommentByStudent({
+          username: data.username,
+          class_id: data.course_id,
+          teacher_name: data.class_teacher,
+          teacher_id: data.teacher_id,
+          memo: data.memo
+        }).then(res => {
+          console.log(res)
+        })
+      } else {
+        this.$message.warning({
+          message: '请输入评价',
+          center: true,
+          duration: 1000
+        })
+      }
+    },
     dateFormat:
       function(row, column) {
         var date = row[column.property]
@@ -54,6 +159,7 @@ export default {
         }
         return moment(date).format('YYYY-MM-DD HH:mm:ss')
       }
+
   }
 }
 </script>
