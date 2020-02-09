e365092971b9822a949b25b68df3382d6bbb4118
x-bessie <qwas002@126.com>
2020-02-09 16:51:30 +0800

    修改一些前端参数

--- eladmin-web/src/settings.js
+++ eladmin-web/src/settings.js
@@ -16,7 +16,7 @@ module.exports = {
    */
   tokenCookieExpires: 1,
   /**
-   * @description 记住密码状态下的密码在Cookie中存储的天数，默认1天s
+   * @description 记住密码状态下的密码在Cookie中存储的天数，默认1天
    */
   passCookieExpires: 1,
   /**
