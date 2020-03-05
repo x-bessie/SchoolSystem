6f17b02b16226066525c2093489e56b9e9fd8c37
x-bessie <qwas002@126.com>
2020-03-06 00:17:17 +0800

    新增批量excel导入方法

--- eladmin/eladmin-system/src/main/java/me/zhengjie/modules/system/repository/RoleRepository.java
+++ eladmin/eladmin-system/src/main/java/me/zhengjie/modules/system/repository/RoleRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+
 import java.util.Set;
 
 /**
@@ -16,6 +17,7 @@ public interface RoleRepository extends JpaRepository<Role, Long>, JpaSpecificat
 
     /**
      * 根据名称查询
+     *
      * @param name /
      * @return /
      */
@@ -23,6 +25,7 @@ public interface RoleRepository extends JpaRepository<Role, Long>, JpaSpecificat
 
     /**
      * 根据用户ID查询
+     *
      * @param id 用户ID
      * @return
      */
@@ -30,16 +33,22 @@ public interface RoleRepository extends JpaRepository<Role, Long>, JpaSpecificat
 
     /**
      * 解绑角色菜单
+     *
      * @param id 菜单ID
      */
     @Modifying
-    @Query(value = "delete from roles_menus where menu_id = ?1",nativeQuery = true)
+    @Query(value = "delete from roles_menus where menu_id = ?1", nativeQuery = true)
     void untiedMenu(Long id);
 
     /**
      * 根据角色权限查询
+     *
      * @param permission /
      * @return /
      */
     Role findByPermission(String permission);
+
+    @Modifying
+    @Query(value = " insert ignore into users_roles (user_id,role_id) VALUES(?1,?2) ", nativeQuery = true)
+    void insertUserId(Long userId, Long roleId);
 }
