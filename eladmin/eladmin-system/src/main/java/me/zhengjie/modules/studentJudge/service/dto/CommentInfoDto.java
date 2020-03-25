e038d7d688ef64e0d98929623ce629570c1e5f7e
x-bessie <qwas002@126.com>
2020-03-25 21:11:35 +0800

    修改字段

--- eladmin/eladmin-system/src/main/java/me/zhengjie/modules/studentJudge/service/dto/CommentInfoDto.java
+++ eladmin/eladmin-system/src/main/java/me/zhengjie/modules/studentJudge/service/dto/CommentInfoDto.java
@@ -1,37 +1,56 @@
 package me.zhengjie.modules.studentJudge.service.dto;
 
 import lombok.Data;
+
 import java.sql.Timestamp;
 import java.io.Serializable;
 import java.util.Date;
 
 /**
-* @author bessie
-* @date 2020-01-30
-*/
+ * @author bessie
+ * @date 2020-01-30
+ */
 @Data
 public class CommentInfoDto implements Serializable {
 
     private Integer id;
 
-    /** 学号 */
+    /**
+     * 学号
+     */
     private String username;
 
-    /** 课程id */
-    private Integer classId;
+    /**
+     * 课程代码
+     */
+    private String classId;
 
-    /** 教师id */
-    private Integer teacherId;
+    /**
+     * 教师工号
+     */
+    private String teacherId;
 
-    /** 教师名称 */
+    /**
+     * 教师名称
+     */
     private String teacherName;
 
-    /** 评价 */
+    /**
+     * 评价
+     */
     private String memo;
 
-    /** 创建时间 */
+    /**
+     * 创建时间
+     */
     private Date createtime;
 
-    /** 状态 */
+    /**
+     * 状态
+     */
     private Integer status;
+    /**
+     * 课程名称
+     */
+    private String className;
 }
