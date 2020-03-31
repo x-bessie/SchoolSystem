28926e9440060c054527a1cd34da5814d075fef4
x-bessie <qwas002@126.com>
2020-03-31 22:53:33 +0800

    添加模糊查询

--- eladmin/eladmin-system/src/main/java/me/zhengjie/modules/grade/service/dto/GradeInfoQueryCriteria.java
+++ eladmin/eladmin-system/src/main/java/me/zhengjie/modules/grade/service/dto/GradeInfoQueryCriteria.java
@@ -22,4 +22,8 @@ public class GradeInfoQueryCriteria{
     /** 模糊 */
     @Query(type = Query.Type.INNER_LIKE)
     private String username;
+
+    /** 模糊 */
+    @Query(type = Query.Type.INNER_LIKE)
+    private String studentName;
 }
